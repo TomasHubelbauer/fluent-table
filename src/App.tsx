508c34048a6cbd72f47dd6f602743b63a8145e99
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-11-27 14:59:21 +0100

    Fix issues with keys

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import "./App.css";
 import React from "react";
-import { Table } from "@stardust-ui/react";
+import { Table, TableRow } from "@stardust-ui/react";
 
 export default function App() {
+  const header = ["a", "b", "c"];
+  const rows = [
+    [1, 2, 3],
+    [4, 5, 6]
+  ];
+
   return (
-    <Table
-      header={["a", "b", "c"]}
-      rows={[
-        [1, 2, 3],
-        [4, 5, 6]
-      ]}
-    />
+    <div className="App">
+      <Table>
+        <TableRow header items={header} />
+        {rows.map((row, index) => (
+          <TableRow key={index} items={row} />
+        ))}
+      </Table>
+    </div>
   );
 }
